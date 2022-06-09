5dc5e5d97cd1999d636d684b4c9a019a399e7e24
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2022-06-10 02:06:03 +0400

    added style every element

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ let id = 1
 let img = document.getElementById("img")
 let title = document.querySelector("h1")
 let main = document.querySelector(".main")
+let prevButton = document.querySelector(".main > #prev")
+let nextButton = document.querySelector(".main > #next")
 img.src = `images/img${id}.jpg`;
 
+// for style
+let titleStyle = title.style
+let imgStyle = img.style
+let mainStyle = main.style
+let prevButtonStyle = prevButton.style
+let nextButtonStyle = nextButton.style
+
+// prev and next functions work change current id
 const prevFunc = () => {
     id <= 1 ? id = 6 : id--
     img.src = `images/img${id}.jpg`;
@@ -15,10 +25,23 @@ const nextFunc = () => {
 }
 
 // write style inside javaScript
-title.style.textAlign= "center"
+titleStyle.textAlign= "center"
 
-img.style.width = "90%"
-img.style.height = "600px"
+imgStyle.width = "80%"
+imgStyle.height = "600px"
 
-main.style.display = "flex"
-main.style.justifyContent ="center"
+mainStyle.display = "flex"
+mainStyle.justifyContent ="center"
+mainStyle.alignItems ="center"
+
+const addButtonStyle = (btn) =>{
+    btn.width = "50px"
+    btn.height = "50px"
+    btn.borderRadius = "30px"
+    btn.border = "none"
+    btn.margin = "10px"
+    btn.cursor = "pointer";
+}
+
+addButtonStyle(prevButtonStyle)
+addButtonStyle(nextButtonStyle)
